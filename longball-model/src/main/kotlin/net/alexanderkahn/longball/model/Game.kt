16a999fed80a35cc59e0af6e05765459a27442f2
Alexander Kahn <akahn@jamasoftware.com>
2017-09-20 09:49:43 -0700

    master:  Prefer OffsetDateTime to ZonedDateTime

--- longball-model/src/main/kotlin/net/alexanderkahn/longball/model/Game.kt
+++ longball-model/src/main/kotlin/net/alexanderkahn/longball/model/Game.kt
@@ -1,6 +1,6 @@
 package net.alexanderkahn.longball.model
 
-import java.time.ZonedDateTime
+import java.time.OffsetDateTime
 import java.util.*
 
-data class Game(val id: UUID, val league: UUID, val awayTeam: UUID, val homeTeam: UUID, val startTime: ZonedDateTime)
+data class Game(val id: UUID, val league: UUID, val awayTeam: UUID, val homeTeam: UUID, val startTime: OffsetDateTime)
