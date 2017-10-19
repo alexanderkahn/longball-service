9ae859723ff72f9b06a2b93b530029392d92ccd2
Alexander Kahn <akahn@jamasoftware.com>
2017-10-19 09:23:41 -0700

    master:  Add tests for Person endpoints

---
+++ integration-test/src/test/kotlin/net/alexanderkahn/longball/itest/PersonControllerIntegrationTest.kt
@@ -0,0 +1,80 @@
+package net.alexanderkahn.longball.itest
+
+import junit.framework.TestCase.assertEquals
+import net.alexanderkahn.longball.presentation.rest.model.PersonAttributes
+import net.alexanderkahn.longball.presentation.rest.model.RequestPerson
+import net.alexanderkahn.longball.presentation.rest.model.ResponsePerson
+import net.alexanderkahn.longball.presentation.rest.model.ResponseTeam
+import net.alexanderkahn.longball.provider.entity.PersonEntity
+import net.alexanderkahn.longball.provider.repository.PersonRepository
+import net.alexanderkahn.service.base.presentation.request.ObjectRequest
+import org.apache.http.HttpStatus
+import org.junit.Before
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+
+class PersonControllerIntegrationTest : AbstractBypassTokenIntegrationTest() {
+
+    @Autowired private lateinit var personRepository: PersonRepository
+
+    @Before
+    fun setUp() {
+        personRepository.deleteAll()
+    }
+
+    @Test
+    fun post() {
+        val requestPerson = RequestPerson("people", PersonAttributes("Vladimir", "Guerrero"))
+        val response = withBypassToken().body(ObjectRequest(requestPerson))
+                .`when`().post("/people")
+                .then().statusCode(HttpStatus.SC_CREATED)
+                .extract().jsonPath().getObject("data", ResponsePerson::class.java)
+
+        assertEquals(requestPerson.type, response.type)
+        assertEquals(requestPerson.attributes, response.attributes)
+    }
+
+    @Test
+    fun postWrongType() {
+        val requestPerson = RequestPerson("purple", PersonAttributes("Valdomar", "Gardrango"))
+        withBypassToken().body(ObjectRequest(requestPerson))
+                .`when`().post("/people")
+                .then().statusCode(HttpStatus.SC_CONFLICT)
+    }
+
+
+    @Test
+    fun delete() {
+        val entityToDelete = PersonEntity("Alex", "Rodriguez")
+        personRepository.save(entityToDelete)
+        withBypassToken().`when`().delete("/people/${entityToDelete.id}")
+                .then().statusCode(HttpStatus.SC_OK)
+
+        withBypassToken().`when`().get("/people/${entityToDelete.id}")
+                .then().statusCode(HttpStatus.SC_NOT_FOUND)
+
+    }
+
+    @Test
+    fun getOne() {
+        val requestPerson = PersonEntity("Ichiro", "Suzuki")
+        personRepository.save(requestPerson)
+        val returnedPerson = withBypassToken().`when`().get("/people/${requestPerson.id}")
+                .then().statusCode(HttpStatus.SC_OK)
+                .extract().jsonPath().getObject("data", ResponsePerson::class.java)
+
+        assertEquals(requestPerson.first, returnedPerson.attributes.first)
+        assertEquals(requestPerson.last, returnedPerson.attributes.last)
+    }
+
+    @Test
+    fun getCollection() {
+        listOf("Yadier", "Bengie", "Jose").map { first -> PersonEntity(first, "Molina") }.forEach { personRepository.save(it) }
+        val getResponse = withBypassToken().`when`().get("/people")
+                .then().statusCode(HttpStatus.SC_OK)
+                .extract().response().jsonPath()
+
+        assertEquals(3, getResponse.getList<ResponseTeam>("data").size)
+    }
+}
