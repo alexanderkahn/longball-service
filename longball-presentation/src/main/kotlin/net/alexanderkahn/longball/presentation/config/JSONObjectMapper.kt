b6789541accc06a7bb960d4ba0f2cf80d6ec09e4
Alexander Kahn <akahn@jamasoftware.com>
2018-03-06 22:31:47 -0800

    65/address-tech-debt:  Move the JsonMixin stuff out of this project, annotate json models directly

--- longball-presentation/src/main/kotlin/net/alexanderkahn/longball/presentation/config/JSONObjectMapper.kt
+++ longball-presentation/src/main/kotlin/net/alexanderkahn/longball/presentation/config/JSONObjectMapper.kt
@@ -1,28 +1,19 @@
 package net.alexanderkahn.longball.presentation.config
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import net.alexanderkahn.service.commons.model.response.body.CollectionResponse
-import net.alexanderkahn.service.commons.model.response.body.ObjectResponse
-import net.alexanderkahn.service.commons.model.response.body.data.ResourceObject
-import net.alexanderkahn.service.commons.model.response.body.meta.CollectionResponseMeta
-import net.alexanderkahn.service.commons.model.response.body.meta.ObjectResponseMeta
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @Component
 open class JsonObjectMapper : ObjectMapper() {
 
-    init {
-        configureMapper()
-        configureMixins()
-    }
-
-    private fun configureMapper() {
+    @PostConstruct
+    fun init() {
         registerModule(KotlinModule())
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         configureDateTimeMapping()
@@ -32,34 +23,5 @@ open class JsonObjectMapper : ObjectMapper() {
     private fun configureDateTimeMapping() {
         registerModule(JavaTimeModule())
         configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-    }
-
-    private fun configureMixins() {
-        configureResponseObjectPropertyOrder()
-    }
-
-    private fun configureResponseObjectPropertyOrder() {
-        // Set the order on response objects so they're consistent across the application
-        // This doesn't matter in terms of application contract, but it's a nice convenience for human readers
-        addMixIn(ResourceObject::class.java, PropertyOrderMixins.ResourceObjectMixin::class.java)
-        addMixIn(ObjectResponse::class.java, PropertyOrderMixins.OkResponseMixin::class.java)
-        addMixIn(CollectionResponse::class.java, PropertyOrderMixins.OkResponseMixin::class.java)
-        addMixIn(ObjectResponseMeta::class.java, PropertyOrderMixins.ObjectResponseMetaMixin::class.java)
-        addMixIn(CollectionResponseMeta::class.java, PropertyOrderMixins.CollectionResponseMetaMixin::class.java)
-    }
-
-    private class PropertyOrderMixins {
-
-        @JsonPropertyOrder("type", "id", "meta", "attributes", "relationships")
-        abstract class ResourceObjectMixin
-
-        @JsonPropertyOrder("meta", "data", "included")
-        abstract class OkResponseMixin
-
-        @JsonPropertyOrder("status", "time")
-        abstract class ObjectResponseMetaMixin
-
-        @JsonPropertyOrder("status", "time", "page")
-        abstract class CollectionResponseMetaMixin
     }
 }
