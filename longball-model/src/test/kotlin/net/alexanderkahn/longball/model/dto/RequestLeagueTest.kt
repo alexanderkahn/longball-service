ca52da8f754c68b12445f85f2a5b6ba663d95ea7
Alexander Kahn <akahn@jamasoftware.com>
2018-03-08 12:59:27 -0800

    65/address-tech-debt:  Move validation tests into the model

---
+++ longball-model/src/test/kotlin/net/alexanderkahn/longball/model/dto/RequestLeagueTest.kt
@@ -0,0 +1,54 @@
+package net.alexanderkahn.longball.model.dto
+
+import net.alexanderkahn.service.commons.model.request.validation.ExpectedType
+import org.apache.commons.lang3.RandomStringUtils
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import javax.validation.Validation
+import javax.validation.Validator
+import javax.validation.constraints.Size
+import kotlin.test.assertEquals
+
+
+internal class RequestLeagueTest {
+
+    @Nested
+    inner class Validates {
+
+        private lateinit var validator: Validator
+
+        @BeforeEach
+        internal fun setUp() {
+            val factory = Validation.buildDefaultValidatorFactory()
+            this.validator = factory.validator
+        }
+
+        @Test
+        internal fun incorrectType() {
+            val invalidType = "large"
+            val league = RequestLeague(invalidType, LeagueAttributes("hello there"))
+            val violations = validator.validate(league)
+            assertEquals(1, violations.size)
+            assertEquals(ExpectedType::class, violations.first().constraintDescriptor.annotation.annotationClass)
+        }
+
+        @Test
+        internal fun belowMinimumLength() {
+            val invalidName = RandomStringUtils.randomAlphabetic(2)
+            val league = RequestLeague("leagues", LeagueAttributes(invalidName))
+            val violations = validator.validate(league)
+            assertEquals(1, violations.size)
+            assertEquals(Size::class, violations.first().constraintDescriptor.annotation.annotationClass)
+        }
+
+        @Test
+        internal fun aboveMaximumLength() {
+            val invalidName = RandomStringUtils.randomAlphabetic(300)
+            val league = RequestLeague("leagues", LeagueAttributes(invalidName))
+            val violations = validator.validate(league)
+            assertEquals(1, violations.size)
+            assertEquals(Size::class, violations.first().constraintDescriptor.annotation.annotationClass)
+        }
+    }
+}
